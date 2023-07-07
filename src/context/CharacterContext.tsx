dfc9ef73378fd1ebcac9f97be8a7032e57865e26
Marek Vaníček <marcovindy@gmail.com>
2023-07-07 17:37:39 +0200

    Add Load More Button

--- src/context/CharacterContext.tsx
+++ src/context/CharacterContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext, useEffect } from 'react'
+import { createContext, useState, useContext, useEffect, useCallback } from 'react'
 import { useQuery } from 'react-query'
 import axios from 'axios'
 import { Character } from '../api/types'
@@ -7,6 +7,7 @@ type CharacterContextData = {
   characters: Character[] | null
   isLoading: boolean
   fetchCharacters: (status?: string, gender?: string) => Promise<void>
+  fetchMoreCharacters: () => Promise<void>
 }
 
 const CharacterContext = createContext<CharacterContextData | undefined>(undefined)
@@ -22,8 +23,9 @@ export const useCharacterContext = () => {
 export const CharacterProvider: React.FC = ({ children }) => {
   const [characters, setCharacters] = useState<Character[] | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [nextPageUrl, setNextPageUrl] = useState<string | null>(null)
 
-  const fetchCharacters = async (status?: string, gender?: string) => {
+  const fetchCharacters = useCallback(async (status?: string, gender?: string) => {
     setIsLoading(true)
     try {
       const params = {
@@ -33,18 +35,37 @@ export const CharacterProvider: React.FC = ({ children }) => {
       const response = await axios.get('https://rickandmortyapi.com/api/character', { params })
       const data = response.data.results
       setCharacters(data)
+      setNextPageUrl(response.data.info.next)
     } catch (error) {
       console.error('Error fetching characters:', error)
     } finally {
       setIsLoading(false)
     }
-  }
+  }, [])
+
+  const fetchMoreCharacters = useCallback(async () => {
+    if (!nextPageUrl) return
+
+    setIsLoading(true)
+    try {
+      const response = await axios.get(nextPageUrl)
+      const newData = response.data.results
+      setCharacters(prevCharacters => (prevCharacters ? [...prevCharacters, ...newData] : newData))
+      setNextPageUrl(response.data.info.next)
+    } catch (error) {
+      console.error('Error fetching more characters:', error)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [nextPageUrl])
 
   useEffect(() => {
     fetchCharacters()
   }, [])
 
   return (
-    <CharacterContext.Provider value={{ characters, isLoading, fetchCharacters }}>{children}</CharacterContext.Provider>
+    <CharacterContext.Provider value={{ characters, isLoading, fetchCharacters, fetchMoreCharacters }}>
+      {children}
+    </CharacterContext.Provider>
   )
 }
--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,5 +1,5 @@
 import { useHistory } from 'react-router-dom'
-import { Typography } from '@mui/material'
+import { Typography, Button, CircularProgress } from '@mui/material'
 import { CharacterFilters } from '../components/CharacterFilters'
 import { CharacterCard } from '../components/CharacterCard'
 import { useCharacterContext } from '../context/CharacterContext'
@@ -8,7 +8,7 @@ import { setFilters } from '../store/reducers/characterSlice'
 
 export const ListPage = () => {
   const history = useHistory()
-  const { characters, isLoading, fetchCharacters } = useCharacterContext()
+  const { characters, isLoading, fetchCharacters, fetchMoreCharacters } = useCharacterContext()
   const dispatch = useDispatch()
 
   const handleCharacterClick = (id: number) => {
@@ -20,17 +20,26 @@ export const ListPage = () => {
     fetchCharacters(status, gender)
   }
 
-  if (isLoading) {
-    return <div>Loading...</div>
+  const handleLoadMore = () => {
+    fetchMoreCharacters()
+  }
+
+  if (isLoading && !characters) {
+    return <CircularProgress />
   }
 
   return (
     <div>
       <CharacterFilters onApplyFilters={handleApplyFilters} />
       {characters && characters.length > 0 ? (
-        characters.map(character => (
-          <CharacterCard key={character.id} character={character} onClick={handleCharacterClick} />
-        ))
+        <>
+          {characters.map(character => (
+            <CharacterCard key={character.id} character={character} onClick={handleCharacterClick} />
+          ))}
+          <Button variant="contained" onClick={handleLoadMore} disabled={isLoading}>
+            Load More
+          </Button>
+        </>
       ) : (
         <Typography>No characters found.</Typography>
       )}
